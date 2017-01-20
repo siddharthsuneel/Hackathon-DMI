43b8d0c730ca64827f459742a0b1ca26d8ababeb
Siddharth Suneel <ssuneel@dminc.com>
2017-01-20 10:55:53 +0530

    Bridging header added.

---
+++ DBeat/DBeat-Bridging-Header.h
@@ -0,0 +1,11 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#ifndef DBeat_Bridging_Header_h
+#define DBeat_Bridging_Header_h
+
+#import <HueSDK_iOS/HueSDK.h>
+
+#endif /* HomeKitDemoApp_Bridgiing_Header_h */
+
---
+++ DBeat/DBeat/dummyHeader.h
@@ -0,0 +1,13 @@
+//
+//  dummyHeader.h
+//  DBeat
+//
+//  Created by Siddharth Suneel on 20/01/17.
+//  Copyright © 2017 DMI. All rights reserved.
+//
+
+#ifndef dummyHeader_h
+#define dummyHeader_h
+
+
+#endif /* dummyHeader_h */
